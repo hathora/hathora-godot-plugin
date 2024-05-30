51aa7bf3e765d365c7df28021381ed414543e7d7
Davide Di Staso <davide.distaso@icloud.com>
2024-05-31 00:09:16 +0200

    Added documentation

--- addons/hathora/sdk/enums.gd
+++ addons/hathora/sdk/enums.gd
@@ -2,12 +2,46 @@ extends RefCounted
 
 class_name Hathora
 
-enum Region { SEATTLE, WASHINGTON_DC, CHICAGO, LONDON, FRANKFURT, MUMBAI, SINGAPORE, SYDNEY, TOKYO, SAO_PAULO, LOS_ANGELES, DALLAS }
-enum TransportType { UDP, TCP, TLS }
-enum Plan { TINY, SMALL, MEDIUM, LARGE }
-enum Visibility { PRIVATE, PUBLIC, LOCAL }
+enum Region {
+	SEATTLE,
+	WASHINGTON_DC,
+	CHICAGO,
+	LONDON,
+	FRANKFURT,
+	MUMBAI,
+	SINGAPORE,
+	SYDNEY,
+	TOKYO,
+	SAO_PAULO,
+	LOS_ANGELES,
+	DALLAS, ##Howdy, Godot developer
+	}
+
+enum TransportType { ## Transport type specifies the underlying communication protocol to the exposed port.
+	UDP, 
+	TCP, 
+	TLS }
+
+enum Plan { ## A plan defines how much CPU and memory is required to run an instance of your game server.
+	TINY, ## shared core, 1gb memory
+	SMALL, ## 1 core, 2gb memory
+	MEDIUM, ## 2 core, 4gb memory
+	LARGE ## 4 core, 8gb memory
+	}
+
+enum Visibility { ## Types of lobbies a player can create.
+PRIVATE, ## The player who created the room must share the roomId with their friends
+PUBLIC, ## Visible in the public lobby list, anyone can join
+LOCAL, ## For testing with a server running locally
+}
+
 enum ProcessStatus { STARTING, RUNNING, DRAINING, STOPPING, STOPPED, FAILED }
-enum RoomStatus { SCHEDULING, ACTIVE, SUSPENDED, DESTROYED }
+
+enum RoomStatus { ## The allocation status of a room.
+	SCHEDULING, 
+	ACTIVE, 
+	SUSPENDED, 
+	DESTROYED }
 
 const ROOM_STATUSES = {
 	RoomStatus.SCHEDULING: "scheduling",
