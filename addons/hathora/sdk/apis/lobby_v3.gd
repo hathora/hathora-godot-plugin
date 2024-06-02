22a63b288ea589a3b413e4eb5cd57dd72e9e8357
Davide Di Staso <davide.distaso@icloud.com>
2024-06-02 17:03:48 +0200

    Changed roomConfig to String and made optional

--- addons/hathora/sdk/apis/lobby_v3.gd
+++ addons/hathora/sdk/apis/lobby_v3.gd
@@ -12,7 +12,7 @@ extends "endpoint.gd"
 ## [br][br]- [param player_token]: [String]
 ## [br][br]- [param visibility]: [enum Hathora.Visibility]
 ## [br][br]- [param region]: [enum Hathora.Region]
-## [br][br]- optionally [param room_config]: [Dictionary]. Optional configuration parameters for the room. It is accessible from the room via [method HathoraSDK.room_v2.get_info].
+## [br][br]- optionally [param room_config]: [String]. Optional configuration parameters for the room. It is accessible from the room via [method HathoraSDK.room_v2.get_info].
 ## [br][br]- optionally [param short_code] for a user-defined identifier for a lobby, for example: [code]"LFG4"[/code]
 ## [br][br]- optionally [param room_id]([ 1 .. 100 ] characters ^[a-zA-Z0-9_-]*$) which overrides the system generated one
 ## [br][br][b]Note:[/b] error will be returned if roomId is not globally unique.
@@ -20,22 +20,21 @@ extends "endpoint.gd"
 ## [br][br]- [code]shortCode[/code]: [String] (<= 100 characters). User-defined identifier for a lobby.
 ## [br][br]- [code]createdAt[/code]: [String] <date-time>. When the lobby was created.
 ## [br][br]- [code]createdBy[/code]: [String]. UserId or email address for the user that created the lobby.
-## [br][br]- [code]roomConfig[/code]: [String] (<= 10000 characters). Optional configuration parameters for the room. Can be any string including stringified JSON. It is accessible from the room via [method HathoraSDK.room_v2.get_info].
+## [br][br]- [code]roomConfig[/code]: [String](<= 10000 characters) or [code]null[/code]. Optional configuration parameters for the room. Can be any string including stringified JSON. It is accessible from the room via [method HathoraSDK.room_v2.get_info].
 ## [br][br]- [code]visibility[/code]: [enum Hathora.Visibility]
 ## [br][br]- [code]region[/code]: [enum Hathora.Region]
 ## [br][br]- [code]roomId[/code]: [String] ([ 1 .. 100 ] characters ^[a-zA-Z0-9_-]*$).
 ## [br][br]- [code]appId[/code]: [String]
-func create(player_token: String, visibility: Hathora.Visibility, region: Hathora.Region, room_config := {}, short_code := "", room_id := ""):
+func create(player_token: String, visibility: Hathora.Visibility, region: Hathora.Region, room_config := "", short_code := "", room_id := ""):
 	client.set_header("Authorization", "Bearer " + player_token)
-	return POST("create", {
+	return POST("create", empty_string_stripped({
 		"visibility": Hathora.VISIBILITY[visibility],
-		"roomConfig": JSON.stringify(room_config),
-		"region": Hathora.REGION_NAMES[region]
-	}, empty_string_stripped({
+		"region": Hathora.REGION_NAMES[region],
+		"roomConfig": room_config,
+	}), empty_string_stripped({
 		"shortCode": short_code,
-		"roomId": room_id
-		})).then(func (result):
-			
+		"roomId": room_id,
+		})).then(func (result):	
 		if result.is_error():
 			return result
 		return result
--- addons/hathora/sdk/apis/room_v2.gd
+++ addons/hathora/sdk/apis/room_v2.gd
@@ -3,7 +3,7 @@ extends "endpoint.gd"
 ## Create a new room for an existing application.
 ## Poll the [method get_info] endpoint to get connection details for an active room.
 ## Takes:
-## [br][br]- [param room_config]: [Dictionary]. Optional configuration parameters for the room. It is accessible from the room via [method get_info].
+## [br][br]- [param room_config]: [String]. Optional configuration parameters for the room. It is accessible from the room via [method get_info].
 ## [br][br]- [param region]: [enum Hathora.Region]
 ## [br][br]- optionally [param room_id][ 1 .. 100 ] characters ^[a-zA-Z0-9_-]*$ which overrides the system generated one
 ## [br][br][b]Note:[/b] error will be returned if roomId is not globally unique.
@@ -21,33 +21,33 @@ extends "endpoint.gd"
 ## [br][br] [code]status[/code]: [enum Hathora.RoomStatus]
 ## [br][br] [code]roomId[/code]: [String]
 ## [br][br] [code]processId[/code]: [String]
-func create(room_config: Dictionary, region: Hathora.Region, room_id = ""):
-	return POST("create", {
-		"roomConfig": JSON.stringify(room_config),
-		"region": Hathora.REGION_NAMES[region]
-	},
+func create(region: Hathora.Region, room_config:= "", room_id = ""):
+	return POST("create", empty_string_stripped({
+		"region": Hathora.REGION_NAMES[region],
+		"roomConfig": room_config,
+	}),
 	empty_string_stripped({"roomId": room_id})).then(func (result):
 		if result.is_error():
 			return result
 		return result
 	)
 
-## Retreive current and historical allocation data for a room.
+## Retrieve current and historical allocation data for a room.
 ## [br][br][br][b]If successful, calls to this endpoint return:[/b]
-## [br][br]- [code]currentAllocation[/code]: [Dictionary] containing:
-## [br]-- [code]unscheduledAt[/code]:[String] <date-time>
-## [br]-- [code]scheduledAt[/code]:[String]
-## [br]-- [code]processId[/code]:[String]. System generated unique identifier to a runtime instance of your game server.
-## [br]-- [code]roomAllocationId[/code]:[String]. System generated unique identifier to an allocated instance of a room.
-## [br][br] [code]status[/code]:[enum Hathora.RoomStatus]
+## [br][br]- [code]currentAllocation[/code]: [Dictionary] or [code]null[/code] containing:
+## [br]-- [code]unscheduledAt[/code]: [String] or [code]null[/code] <date-time>
+## [br]-- [code]scheduledAt[/code]: [String] <date-time>
+## [br]-- [code]processId[/code]: [String]. System generated unique identifier to a runtime instance of your game server.
+## [br]-- [code]roomAllocationId[/code]: [String]. System generated unique identifier to an allocated instance of a room.
+## [br][br] [code]status[/code]: [enum Hathora.RoomStatus]
 ## [br][br]- [code]allocations[/code]: [Array] of dictionaries. Each element contains:
-## [br]-- [code]unscheduledAt[/code]:[String] <date-time>
-## [br]-- [code]scheduledAt[/code]:[String]
-## [br]-- [code]processId[/code]:[String]. System generated unique identifier to a runtime instance of your game server.
-## [br]-- [code]roomAllocationId[/code]:[String]. System generated unique identifier to an allocated instance of a room.
-## [br][br] [code]roomConfig[/code]:[String]
-## [br][br] [code]roomId[/code]:[String]
-## [br][br] [code]appId[/code]:[String]
+## [br]-- [code]unscheduledAt[/code] or [code]null[/code]:[String] <date-time>
+## [br]-- [code]scheduledAt[/code]: [String] <date-time>
+## [br]-- [code]processId[/code]: [String]. System generated unique identifier to a runtime instance of your game server.
+## [br]-- [code]roomAllocationId[/code]: [String]. System generated unique identifier to an allocated instance of a room.
+## [br][br] [code]roomConfig[/code]: [String](<= 10000 characters) or [code]null[/code]
+## [br][br] [code]roomId[/code]: [String]
+## [br][br] [code]appId[/code]: [String]
 func get_info(room_id: String):
 	return GET("info/" + room_id).then(func (result):
 		if result.is_error():
@@ -60,9 +60,9 @@ func get_info(room_id: String):
 ## [br][br][Array] of dictionaries. Each element contains:
 ## [br]-- [code]appId[/code]: [String]
 ## [br]-- [code]roomId[/code]: [String]
-## [br]-- [code]roomConfig[/code]: [String]
+## [br]-- [code]roomConfig[/code]: [String] or [code]null[/code]
 ## [br]-- [code]status[/code]: [enum Hathora.RoomStatus]
-## [br]-- [code]currentAllocation[/code]: [Dictionary]
+## [br]-- [code]currentAllocation[/code]: [Dictionary] or [code]null[/code]. See [method get_info] for the contents.
 func get_active(process_id: String):
 	return GET("list/" + process_id + "/active").then(func (result):
 		if result.is_error():
@@ -70,7 +70,7 @@ func get_active(process_id: String):
 		return result
 		)
 
-## Get all inactive rooms for a given [param process_id].
+## Get all inactive rooms for a given [param process_id]. See [method get_active] for the contents.
 func get_inactive(process_id: String):
 	return GET("list/" + process_id + "/inactive").then(func (result):
 		if result.is_error():
@@ -96,6 +96,11 @@ func suspend(room_id: String):
 		)
 
 ## Poll this endpoint to get connection details to a room. Clients can call this endpoint without authentication.
+## [br][br][br][b]If successful, calls to this endpoint return:[/b]
+## [br][br][code]additionalExposedPorts[/code]: [Array] of dictionaries. Connection details for up to 2 exposed ports. See [method create] for the contents.
+## [br][br][code]exposedPort[/code]: [Dictionary] or [code]null[/code]. Connection details for an active process. See [method create] for the contents.
+## [br][br]- [code]status[/code]: [enum Hathora.ProcessStatus]. Status of the process.
+## [br][br]- [code]roomId[/code]: [String] [ 1 .. 100 ] characters ^[a-zA-Z0-9_-]*$
 func get_connection_info(room_id: String):
 	return GET("connectioninfo/" + room_id).then(func (result):
 		if result.is_error():
