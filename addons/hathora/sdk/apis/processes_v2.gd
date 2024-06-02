03728b1e5cb5f8d191f4926a7ec23c512bb53812
Davide Di Staso <davide.distaso@icloud.com>
2024-06-02 17:04:45 +0200

    Documentation for null responses

--- addons/hathora/sdk/apis/processes_v2.gd
+++ addons/hathora/sdk/apis/processes_v2.gd
@@ -1,16 +1,18 @@
 ## Operations to create, manage, and connect to processes.
+
 extends "endpoint.gd"
+
 ## Get details for a process.
 ## [br][br][br][b]If successful, calls to this endpoint return:[/b]
 ## [br][br][code]status[/code]: [enum Hathora.ProcessStatus]. Status of the process.
 ## [br][br][code]roomsAllocated[/code]: [float]. Tracks the number of rooms that have been allocated to the process.
-## [br][br][code]terminatedAt[/code]: [String]. When the process has been terminated.
-## [br][br][code]stoppingAt[/code]: [String]. When the process is issued to stop. Used to determine when billing should stop.
-## [br][br][code]startedAt[/code]: [String]. When the process bound to the specified port. Used to determine when billing should start.
-## [br][br][code]createdAt[/code]: [String]. When the process started being provisioned.
+## [br][br][code]terminatedAt[/code]: [String] or [code]null[/code]. When the process has been terminated.
+## [br][br][code]stoppingAt[/code]: [String] or [code]null[/code]. When the process is issued to stop. Used to determine when billing should stop.
+## [br][br][code]startedAt[/code]: [String] or [code]null[/code]. When the process bound to the specified port. Used to determine when billing should start.
+## [br][br][code]createdAt[/code]: [String] <date-time>. When the process started being provisioned.
 ## [br][br][code]roomsPerProcess[/code]: [float][ 1 .. 10000 ]. Governs how many rooms can be scheduled in a process.
 ## [br][br][code]additionalExposedPorts[/code]: [Array] of dictionaries. Connection details for up to 2 exposed ports.
-## [br][br][code]exposedPort[/code]: [Dictionary]. Connection details for an active process.
+## [br][br][code]exposedPort[/code]: [Dictionary] or [code]null[/code]. Connection details for an active process.
 ## [br][br][code]region[/code]: [enum Hathora.Region].
 ## [br][br][code]processId[/code]: [String]. System generated unique identifier to a runtime instance of your game server.
 ## [br][br][code]deploymentId[/code]: [float]. System generated id for a deployment. Increments by 1.
