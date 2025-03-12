31e0dad2b343f4bb83635e4884c77a5f22ad5dac
Justin <jchu231@gmail.com>
2025-03-11 17:45:56 -0700

    updating pludin auth to use orgTokens isntead of storing auth0 token

--- addons/hathora/plugin/auth0/auth0Client.gd
+++ addons/hathora/plugin/auth0/auth0Client.gd
@@ -6,6 +6,7 @@ const DotEnv = preload("../dotenv.gd")
 const CLIENT_ID = "tWjDhuzPmuIWrI8R9s3yV3BQVw2tW0yq";
 const AUTH0_DOMAIN = "https://auth.hathora.com";
 const AUDIENCE_URI = "https://cloud.hathora.com";
+const HATHORA_API_BASE_URL = "https://api.hathora.dev";
 
 var isReady = false
 var device_code: String
@@ -80,26 +81,131 @@ func poll_for_token(login_complete_cb: Callable) -> void:
 func _on_token_received(result, response_code, headers, body, login_complete_cb: Callable) -> void:
 	if response_code == 200:
 		var j = JSON.new()
-		var response = j.parse(body.get_string_from_utf8())
+		var parse_result = j.parse(body.get_string_from_utf8())
+		if parse_result != OK:
+			print("[HATHORA] Failed to parse token response JSON")
+			_cleanup_and_fail(login_complete_cb)
+			return
+			
+		if not j.data.has("access_token") or not j.data.has("id_token"):
+			print("[HATHORA] Expected token fields missing from response")
+			_cleanup_and_fail(login_complete_cb)
+			return
+			
 		var access_token = j.data["access_token"]
 		var id_token = j.data["id_token"]
-
-		# Use the access_token and id_token as needed
-		print("[HATHORA] Storing access token in the config file as HATHORA_DEVELOPER_TOKEN")
-		DotEnv.add("HATHORA_DEVELOPER_TOKEN", access_token)
 		
-		# Login completed, trigger callback
-		if login_complete_cb:
-			login_complete_cb.call(true)
-
+		print("[HATHORA] Authentication complete, beginning process to create Hathora API token")
+		
 		# Stop polling for token
-		interval_timer.stop()
-		interval_timer.queue_free()
+		_cleanup_timer()
+		
+		get_orgs.bind(access_token, login_complete_cb).call()
+
 	elif (response_code == 400 || response_code == 403):
 		# Token not yet available, continue polling
 		pass
 	else:
 		print("[HATHORA] Token request failed with response code:", response_code)		
-		# Stop polling for token
+		_cleanup_and_fail(login_complete_cb)
+
+func _cleanup_timer() -> void:
+	if interval_timer:
 		interval_timer.stop()
 		interval_timer.queue_free()
+		interval_timer = null
+
+func _cleanup_and_fail(login_complete_cb: Callable) -> void:
+	_cleanup_timer()
+	if login_complete_cb:
+		login_complete_cb.call(false)
+
+func get_orgs(access_token: String, login_complete_cb: Callable) -> void:
+	var url = HATHORA_API_BASE_URL + "/orgs/v1"
+	
+	var http_request = HTTPRequest.new()
+	add_child(http_request)
+	var auth_header = "Authorization: Bearer " + access_token
+	var err = http_request.request(url, ["Content-Type: application/json", auth_header], HTTPClient.METHOD_GET)
+	if err != OK:
+		print("[HATHORA] Error making request to get orgs:", err)
+		_cleanup_and_fail(login_complete_cb)
+		http_request.queue_free()
+		return
+		
+	http_request.request_completed.connect(
+		func(result, response_code, headers, body):
+			create_org_token(result, response_code, headers, body, access_token, login_complete_cb)
+			http_request.queue_free()  # Clean up the request node
+	)
+
+func create_org_token(result, response_code, headers, body, access_token: String, login_complete_cb: Callable) -> void:
+	if response_code != 200:
+		print("[HATHORA] Get orgs request failed with response code:", response_code)
+		_cleanup_and_fail(login_complete_cb)
+		return
+		
+	var j = JSON.new()
+	var parse_result = j.parse(body.get_string_from_utf8())
+	if parse_result != OK:
+		print("[HATHORA] Failed to parse orgs response JSON")
+		_cleanup_and_fail(login_complete_cb)
+		return
+	
+	if not j.data.has("orgs") or j.data["orgs"].size() == 0:
+		print("[HATHORA] No organizations found in account")
+		_cleanup_and_fail(login_complete_cb)
+		return
+		
+	var org_id = j.data["orgs"][0]["orgId"]
+	var org_scopes = j.data["orgs"][0]["scopes"]
+	
+	var url = HATHORA_API_BASE_URL + "/tokens/v1/orgs/" + org_id + "/create"
+	var reqBody = {
+		"scopes": org_scopes,
+		"name": "godot-plugin-token_" + get_formatted_datetime()
+	}
+	
+	var http_request = HTTPRequest.new()
+	add_child(http_request)
+	var auth_header = "Authorization: Bearer " + access_token
+	var err = http_request.request(url, ["Content-Type: application/json", auth_header], HTTPClient.METHOD_POST, JSON.stringify(reqBody))
+	if err != OK:
+		print("[HATHORA] Error making request to create token:", err)
+		_cleanup_and_fail(login_complete_cb)
+		http_request.queue_free()
+		return
+		
+	http_request.request_completed.connect(
+		func(result, response_code, headers, body):
+			_create_org_token_completed(result, response_code, headers, body, login_complete_cb)
+			http_request.queue_free()  # Clean up the request node
+	)
+
+func _create_org_token_completed(result, response_code, headers, body, login_complete_cb: Callable) -> void:
+	if response_code >= 200 && response_code < 300:
+		var j = JSON.new()
+		var parse_result = j.parse(body.get_string_from_utf8())
+		if parse_result != OK:
+			print("[HATHORA] Failed to parse token creation response JSON")
+			_cleanup_and_fail(login_complete_cb)
+			return
+			
+		if not j.data.has("plainTextToken"):
+			print("[HATHORA] Token field missing from response")
+			_cleanup_and_fail(login_complete_cb)
+			return
+			
+		var org_token = j.data["plainTextToken"]
+		print("[HATHORA] Successfully created and stored Hathora API token")
+		DotEnv.add("HATHORA_DEVELOPER_TOKEN", org_token)
+		
+		# Login completed, trigger callback
+		login_complete_cb.call(true)
+	else:
+		print("[HATHORA] Create Hathora API token request failed with response code:", response_code)
+		_cleanup_and_fail(login_complete_cb)
+
+func get_formatted_datetime() -> String:
+	var dt = Time.get_datetime_dict_from_system()
+	return "%02d-%02d-%04d_%02d:%02d:%02d" % [dt.month, dt.day, dt.year, dt.hour, dt.minute, dt.second]
